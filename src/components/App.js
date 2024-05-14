db05832ae19194e03666d00b3f1e8de0f0af4435
s-k3nyyy <158499263+s-k3nyyy@users.noreply.github.com>
2024-05-14 20:22:42 +0300

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import  Home from "./Home;"
+import About from "./About"
 function NavBar() {
   return (
     <nav>
@@ -12,21 +13,29 @@ function Home() {
   return (
     <div id="home">
       <h1>Home</h1>
+
     </div>
   );
 }
+function About() {
+  return (
+    <div id="about">
+      <h1>about</h1>
 
-{/* write an <About> component here */}
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      < Home/>
+      < About />
     </div>
   );
 }
 
 export default App;
+
